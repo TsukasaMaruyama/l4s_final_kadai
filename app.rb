29d7ea2e1eb16e47ab5bf774ec11addc6ac83b87
m1610601 <tsukasa.php@gmail.com>
2018-10-01 18:14:39 +0000

    fix

--- app.rb
+++ app.rb
@@ -5,10 +5,11 @@ require 'sinatra/reloader' if development?
 
 
 post '/mokmoks/create' do
-  challenge = params[:challenge]
-  res = {challenge: challenge}
+  text = JSON.parse request.body.read
+  res = {challenge: text["challenge"]}
   json res
 end
+
 
 get '/mokmoks/create' do
   challenge = {challenge:"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P"}
