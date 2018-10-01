84597d4368584b31b7d644d6d37e0b574f8a3478
m1610601 <tsukasa.php@gmail.com>
2018-10-01 17:44:34 +0000

    fix

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'sinatra/reloader' if development?
 
 
 post '/mokmoks/create' do
-  challenge = {"challenge":"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P"}
+  res = {"challenge":"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P"}
   json challenge
 end
 
