4b4e47c39c16a826c3ed57a822888f6c75afd6ac
m1610601 <tsukasa.php@gmail.com>
2018-10-01 19:09:56 +0000

    fix

--- app.rb
+++ app.rb
@@ -3,15 +3,73 @@ Bundler.require
 require 'sinatra/json'
 require 'sinatra/reloader' if development?
 
+require 'net/http'
+require 'uri'
+
 
 post '/mokmoks/create' do
-  text = JSON.parse request.body.read
-  res = {challenge: text["challenge"]}
+  params = JSON.parse request.body.read
+  res = {challenge: params["challenge"]}
   json res
 end
 
+def talk(content)
+  uri = URI.parse("https://hooks.slack.com/services/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")
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true # HTTPSでよろしく
+  req = Net::HTTP::Post.new(uri.request_uri)
+
+  req["Content-Type"] = "application/json" # httpリクエストヘッダの追加
+  payload = content.to_json
+  req.body = payload # リクエストボデーにJSONをセット
+  https.request(req)
+end
+
+post 'event_catch' do
+  talk({'text': "イベントをキャッチしました"})
+end
 
 get '/mokmoks/create' do
+  content = {
+    "text": "Would you like to play a game?",
+    "attachments": [
+      {
+        "text": "Choose a game to play",
+        "fallback": "You are unable to choose a game",
+        "callback_id": "participate_mokmok",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "actions": [
+          {
+            "name": "game",
+            "text": "Chess",
+            "type": "button",
+            "value": "chess"
+          },
+          {
+            "name": "game",
+            "text": "Falken's Maze",
+            "type": "button",
+            "value": "maze"
+          },
+          {
+            "name": "game",
+            "text": "Thermonuclear War",
+            "style": "danger",
+            "type": "button",
+            "value": "war",
+            "confirm": {
+              "title": "Are you sure?",
+              "text": "Wouldn't you prefer a good game of chess?",
+              "ok_text": "Yes",
+              "dismiss_text": "No"
+            }
+          }
+        ]
+      }
+    ]
+  }
+  talk(content)
   challenge = {challenge:"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P"}
   json challenge
 end
