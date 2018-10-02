188e8ecbef05bc87e3b39aafca60cae9de587db6
m1610601 <tsukasa.php@gmail.com>
2018-10-02 18:58:28 +0000

    fix

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ post '/mokmoks/create' do
   res = {challenge: params["challenge"]}
   user_info = exportMemberInfo(WORKSPACE_TOKEN, params['event']['user'])
   user_name = user_info["profile"]["display_name"]
-  text = params["text"]
+  text = params["event"]["text"]
   talk({"text": "Hello! " + user_name + " " + text})
 end
 
