f72f9c41939a5b7c788d0e44ebfaadbdcc463939
m1610601 <tsukasa.php@gmail.com>
2018-10-03 18:39:20 +0000

    fix

--- app.rb
+++ app.rb
@@ -122,7 +122,8 @@ end
 post '/event_catch_post' do
   payload = JSON.parse(params["payload"])
   #  user_name= params["challenge"]
-  talk({"text": params["payload"]})
+  # talk({"text": params["payload"]})
+  talk({"text": payload["user"]["id"]})
   user_name = exportMemberName(WORKSPACE_TOKEN, payload["user"]["id"])
   talk({"text": user_name})
 end
